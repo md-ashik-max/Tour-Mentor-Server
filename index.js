b1476e6bb03c67c8d01f23f90cec55636c2a1709
md-ashik-max <ar1214697@gmail.com>
2024-04-28 21:33:14 +0600

    added findone data function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,7 +36,14 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
           })
-
+ 
+          app.get('/spot/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)}
+            const result =await spotCollection.findOne(query)
+            console.log(result)
+            res.send(result)
+        })  
 
         app.post('/spot', async (req, res) => {
             const newSpot = req.body;
